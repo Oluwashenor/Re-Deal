146de0a523e9135325a2407f2e7e24e080439c35
Kitan 001 <ewuolaoluwakitan@gmail.com>
2022-09-02 11:37:41 +0100

    2nd Commit

--- register.php
+++ register.php
@@ -1,25 +1,45 @@
 <?php
 session_start();
 include './config/database.php';
+
+
+
 if (isset($_POST['submit'])){
+    if(empty($_POST['username'])){
+    echo "<script>alert('Username is required')</script>";
+}elseif ( ! filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+    echo "<script>alert('Valid Email Required')</script>";
+}elseif(strlen($_POST['password']) < 6 ){
+    echo "<script>alert('password must be at least 6 characters')</script>";
+
+}elseif(! preg_match('/[a-z]/i', $_POST['password'])){
+    echo "<script>alert('password must contain at least a letter ')</script>" ;
+}elseif(! preg_match('/[0-9]/', $_POST['password'])){
+    echo "<script>alert ('password must contain at least a number')</script>";
+}elseif($_POST['password'] !== $_POST['password_confirmation']){
+    echo "<script>alert('Passwords must match')</script>";
+}else{
     $username= $_POST['username'];
     $email= $_POST['email'];
     $password= $_POST['password'];
+    $password_confirmation= $_POST['password_confirmation'];
+  
 
-
-    $query= mysqli_query($conn, "select * from `users` where email= '$email'");
+    $sqlQuery = "select * from `users` where email= '$email'";
+    $query= mysqli_query($conn, $sqlQuery);
     
     if (mysqli_num_rows($query) >0){
         echo "<script>alert('email already exists')</script>";
     }else{
+        $password= password_hash($_POST['password'], PASSWORD_DEFAULT);
         $sql= "insert into `users` (username, email, password) values ('$username','$email', '$password' )";
         $result= mysqli_query($conn, $sql);
         if($result){
             echo 'User Registered';
         }else{
-            die(mysqli_error($con));
+           die(mysqli_error($con));
         }
-    }
+    }}
 
 }
 
@@ -76,16 +96,21 @@ if (isset($_POST['submit'])){
 <body>
     <div class="img"></div>
     <form action="" method="post">
+        <h2>RE-IDEA</h2>
         <h3>Register Account</h3>
         <label for="email">Email:</label><br>
-        <input type="email" required name="email" id=""><br>
+        <input type="email"  name="email" id=""><br>
         <label for="name" >Username:</label><br>
-        <input type="text" required name="username" id=""><br>
+        <input type="text"  name="username" id=""><br>
         <label for="password">Password:</label><br>
-        <input type="password" required name="password" id=""><br>
+        <input type="password"  name="password" id=""><br>
+        <label for="password-confirm">Confirm Password:</label><br>
+        <input type="password"  name="password_confirmation" id=""><br>
        
         <input type="submit"  value="Submit" name="submit">
         <p>Already have an account? <a href="login.php">Login</a> </p>
     </form>
+
+    
 </body>
 </html>
